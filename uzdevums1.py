1d25f712b20f43f29c783f4e3f2f0a950dd68045
MarkussApenitis <markuss.apenitis@grobinasskola.lv>
2023-11-23 11:38:13 +0200

    Pirmais uzdevums pabeigts

---
+++ uzdevums1.py
@@ -0,0 +1,7 @@
+ievade=int(input("Ievadi skaitli"))
+summa=0
+for skaitlis in range (1,ievade+1):
+    summa=summa+skaitlis
+print("Skaitļu summa no 1 līdz Jūsu ievadītajam skaitlim ir ",summa)
+
+    
